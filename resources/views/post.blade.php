fa47cd7db3f7ef7960dfcee2ccca08a851ceec48
unknown <gunturpongkyp@gmail.com>
2021-12-03 04:47:29 +0700

    video 8 - Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,9 @@
 @section('container')
 
 <article>
-<h2>{{ $post["title"] }}</h2>
-<h5>by: {{ $post["author"] }}</h5>
-<p>{{  $post["body"] }}</p>
+<h2>{{ $post->title }}</h2>
+<p>by Guntur Pongky Prayusti in Programming {{<a href="/categories/{{ $post->category->slug }}"> $post->category->name }}< /   a></p>
+{!!  $post->body !!}
 </article>
     
 <a href="/blog">Back to Posts</a>
