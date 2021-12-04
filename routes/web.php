d63598d25d98b268bfb49fb6b138050689d45a9f
unknown <gunturpongkyp@gmail.com>
2021-12-04 13:35:50 +0700

    Video 14 View Login & Registration
    Ada kodingan yg salah di commit sebelumnya di video 14

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
+use App\Models\Post;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
@@ -8,8 +9,7 @@ use App\Http\Controllers\RegisterController;
 
 
 use App\Models\Category;
-use GuzzleHttp\Middleware;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,44 +21,33 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('home', [
         "title" => "Home",
         'active' => 'categories'
     ]);
 });
-
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
         'active' => 'categories',
-        "name" => "Guntur Pongky Prayusti",
+        "name" => "Guntur pongky Prayusti",
         "email" => "gunturpongkyp@gmail.com",
         "image" => "1.jpeg"
     ]);
 });
-
-
-
-
 Route::get('/blog', [PostController::class, 'index']);
 Route::get('blog/{post:slug}', [PostController::class, 'show']);
-
 Route::get('/categories', function () {
     return view('categories', [
         'title' => 'Post Categories',
         'active' => 'categories', 
         'categories' => Category::all()
     ]);
+}); 
 });
 
 
-Route::get('/login', [LoginController::class ,'index'])->name('login')->middleware('guest');
-Route::post('/login', [LoginController::class ,'authenticate']);
-Route::post('/logout', [LoginController::class ,'logout']);
+Route::get('/login', [LoginController::class ,'index']);
 
-Route::get('/register', [RegisterController::class ,'index'])->middleware('geust');
-Route::post('/register', [RegisterController::class ,'store']);
-
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/register', [RegisterController::class ,'index']);
