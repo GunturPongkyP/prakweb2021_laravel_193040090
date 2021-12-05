a12af2301e3d24b732e1c59c14a1802cfcf8a7be
unknown <gunturpongkyp@gmail.com>
2021-12-06 02:13:54 +0700

    Video 21 - Upload Image

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,10 +1,11 @@
 <?php
+
 namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Category;
 use App\Models\Post;
-use PDO;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,37 +16,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        
 
         User::create([
-            'name' =>'Guntur Pongky P',
+            'name' =>'Guntur Pongky Prayusti ',
             'email' => 'gunturpongkyp@gmail.com',
             'password' => bcrypt('12345')
         ]);
 
-
         // User::create([
-        //     'name' =>'Guntur Pongky P',
-        //     'email' => 'gunturpongkypi@gmail.com',
+        //     'name' =>' 'Guntur Pongky Prayusti',
+        //     'email' => 'gunturpongkyp@gmail.com',
         //     'password' => bcrypt('12345')
         // ]);
-
-        // User::create([
-        //     'name' =>' Utuy',
-        //     'email' => 'utuy@gmail.com',
-        //     'password' => bcrypt('12345')
-        // ]);
-
+        
         User::factory(3)->create();
 
-        User::create([
-            'name' =>' Utuy',
-            'email' => 'gunturpongkyp@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
         Category::create([
-            'name'=> 'Web Programing',
-            'slug'=> 'web Programing'
             'name' => 'Web Programming',
             'slug' => 'web-programming'
         ]);
@@ -54,66 +41,6 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug'=> 'judul-pertama',
-            'excerpt' =>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ipsum earum deleniti voluptatem 
-            pariatur laboriosam numquam officia. Eius, nulla excepturi voluptas unde incidunt dolores pariatur ad id quisquam inventore.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ipsum earum deleniti voluptatem 
-            pariatur laboriosam numquam officia. Eius, nulla excepturi voluptas unde incidunt dolores pariatur ad id quisquam inventore. 
-            Repellat iure sunt molestias architecto, vel ad eum, voluptatum, deleniti aliquid illum nam ex similique molestiae explicabo
-            fuga nemo laborum provident minima reiciendis? Voluptatem earum, minus dolor possimus animi dignissimos 
-            temporibus nihil modi facere rerum consectetur hic eius unde voluptatum reprehenderit itaque consequatur.
-            Commodi ut suscipit dolor aliquam corrupti velit ducimus excepturi quisquam, numquam eos iste in maiores architecto reprehenderit, 
-            tenetur atque voluptas soluta ea facere pariatur debitis dolorem non quae.',
-            'category_id' => 1,
-            'user_id' =>1
-        ]);
-        Post::create([
-            'title' => 'Judul Kedua',
-            'slug'=> 'judul-ke-dua',
-            'excerpt' =>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ipsum earum deleniti voluptatem 
-            pariatur laboriosam numquam officia. Eius, nulla excepturi voluptas unde incidunt dolores pariatur ad id quisquam inventore.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ipsum earum deleniti voluptatem 
-            pariatur laboriosam numquam officia. Eius, nulla excepturi voluptas unde incidunt dolores pariatur ad id quisquam inventore. 
-            Repellat iure sunt molestias architecto, vel ad eum, voluptatum, deleniti aliquid illum nam ex similique molestiae explicabo
-            fuga nemo laborum provident minima reiciendis? Voluptatem earum, minus dolor possimus animi dignissimos 
-            temporibus nihil modi facere rerum consectetur hic eius unde voluptatum reprehenderit itaque consequatur.
-            Commodi ut suscipit dolor aliquam corrupti velit ducimus excepturi quisquam, numquam eos iste in maiores architecto reprehenderit, 
-            tenetur atque voluptas soluta ea facere pariatur debitis dolorem non quae.',
-            'category_id' => 1,
-            'user_id' =>1
-        ]);
-        Post::create([
-            'title' => 'Judul Ketiga',
-            'slug'=> 'judul-ke-tiga',
-            'excerpt' =>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ipsum earum deleniti voluptatem 
-            pariatur laboriosam numquam officia. Eius, nulla excepturi voluptas unde incidunt dolores pariatur ad id quisquam inventore.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ipsum earum deleniti voluptatem 
-            pariatur laboriosam numquam officia. Eius, nulla excepturi voluptas unde incidunt dolores pariatur ad id quisquam inventore. 
-            Repellat iure sunt molestias architecto, vel ad eum, voluptatum, deleniti aliquid illum nam ex similique molestiae explicabo
-            fuga nemo laborum provident minima reiciendis? Voluptatem earum, minus dolor possimus animi dignissimos 
-            temporibus nihil modi facere rerum consectetur hic eius unde voluptatum reprehenderit itaque consequatur.
-            Commodi ut suscipit dolor aliquam corrupti velit ducimus excepturi quisquam, numquam eos iste in maiores architecto reprehenderit, 
-            tenetur atque voluptas soluta ea facere pariatur debitis dolorem non quae.',
-            'category_id' => 2,
-            'user_id' =>1
-        ]);
-        Post::create([
-            'title' => 'Judul Ke Empat',
-            'slug'=> 'judul-ke-empat',
-            'excerpt' =>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ipsum earum deleniti voluptatem 
-            pariatur laboriosam numquam officia. Eius, nulla excepturi voluptas unde incidunt dolores pariatur ad id quisquam inventore.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique ipsum earum deleniti voluptatem 
-            pariatur laboriosam numquam officia. Eius, nulla excepturi voluptas unde incidunt dolores pariatur ad id quisquam inventore. 
-            Repellat iure sunt molestias architecto, vel ad eum, voluptatum, deleniti aliquid illum nam ex similique molestiae explicabo
-            fuga nemo laborum provident minima reiciendis? Voluptatem earum, minus dolor possimus animi dignissimos 
-            temporibus nihil modi facere rerum consectetur hic eius unde voluptatum reprehenderit itaque consequatur.
-            Commodi ut suscipit dolor aliquam corrupti velit ducimus excepturi quisquam, numquam eos iste in maiores architecto reprehenderit, 
-            tenetur atque voluptas soluta ea facere pariatur debitis dolorem non quae.',
-            'category_id' => 2,
-            'user_id' =>2
-        ]);
         Post::Factory(20)->create();
 
         // Post::create([
